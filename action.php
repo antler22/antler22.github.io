f051185fc4b0b2311dff5580888262fdde7ca5bf
antler22 <nick.jacobsen@okstate.edu>
2017-04-10 15:15:20 -0500

    php madness

---
+++ action.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+Farm name <?php echo $_POST["Farm Name"]; ?><br>
+Location <?php echo $_POST["Location"]; ?><br>
+Machine Serial No. <?php echo $_POST["Serial"]; ?><br>
+Dealership <?php echo $_POST["Dealer"]; ?><br>
+
+</body>
+</html>
